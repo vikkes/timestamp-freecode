fa53c95e2a21a004f08d475c465077d1ebe78cbd
Viktor <vikkes@gmail.com>
2016-08-03 18:32:21 +0000

    final

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var moment =require("moment");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:str', function (req, res) {
+   var myDate;
+  if(/^\d{8,}$/.test(req.params.str)) {
+    myDate = moment(req.params.str, "X");
+  } else {
+    myDate = moment(req.params.str, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
 });
 
 app.listen(8080, function () {
